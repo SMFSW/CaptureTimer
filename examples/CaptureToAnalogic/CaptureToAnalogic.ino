d4c8abadd7ddec6cb6b6e46ec8bbdc990de1c4f4
SMFSW <SMFSW@users.noreply.github.com>
2016-11-24 21:22:34 +0100

    v0.2b2: updated advanced example to compile on AVR platforms without any changes

--- examples/CaptureToAnalogic/CaptureToAnalogic.ino
+++ examples/CaptureToAnalogic/CaptureToAnalogic.ino
@@ -6,42 +6,52 @@
 	This example code is in the public domain.
 
 	created 24 November 2016
+	latest mod 24 November 2016
 	by SMSFSW
 */
 
 #include <CaptureTimer.h>
 
 
-#define acqPeriod		50		//!< Acquisition period
+#define acqPeriod		50			//!< Acquisition period
 
-#define maxTicks		1000	//!< maximum ticks expected in an acquisition window
+#define maxTicks		1000		//!< maximum ticks expected in an acquisition window
 
-#define dacOUTPin		DAC0	//!< DAC output pin (can also be a PWM pin, even if there could be less interest)
+#ifdef __AVR__
+	#define outPin		3			//!< PWM output pin (on AVR: less interest, but demonstrates the use)
+#else
+	#define outPin		DAC0		//!< DAC output pin
+#endif
 
-#define icINPin			2		//!< Pin used for Input Capture (ticks count)
+#define icINPin			2			//!< Pin used for Input Capture (ticks count)
 
 /*** GLOBAL VARS ***/
+uint16_t maxDAC = 4095;
 uint16_t valDAC = 0;
 
 
 void setup()
 {
 	Serial.begin(115200);
-	while (!Serial);	// Waiting for Serial to be ready
+	while (!Serial);	// Waiting for Serial to be ready (only needed on particular boards)
 
 	CaptureTimer::init(icINPin, acqPeriod);
 
-	analogWriteResolution(12);
+	#ifndef __AVR__
+		analogWriteResolution(12);	// set DAC resolution to 12 bits for higher accuracy
+	#else
+		maxDAC = 255;	// If on AVR, most probably using a PWM pin, resolution is 8bits
+	#endif
 }
 
 void loop()
 {
 	uint16_t ticks;
 
-	if (CaptureTimer::getTicks(&ticks) == true)		// new datas acquired (happens once every (acqPeriod)ms)
+	if (CaptureTimer::getTicks(&ticks) == true)			// new datas acquired (happens once every (acqPeriod)ms)
 	{
-		ticks = min(ticks, maxTicks);				// clamp value against max expected
-		valDAC = map(ticks, 0, maxTicks, 0, 4095);	// map ticks to DAC range
-		analogWrite(dacOUTPin, valDAC);				// ~0.58V to ~2.8V is expected on Due DAC for example
+		ticks = min(ticks, maxTicks);					// clamp value against max expected
+		valDAC = map(ticks, 0, maxTicks, 0, maxDAC);	// map ticks to DAC/PWMDutyCycle range
+		analogWrite(outPin, valDAC);					// ~0.58V to ~2.8V is expected on Due DAC for example
 	}
 }
